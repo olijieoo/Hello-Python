2379515d608a78547b0903a57ca1163df76b3337
jieli <lijie_1991@hotmail.com>
2016-05-15 02:02:36 -0700

    My first Code fight answer !

---
+++ HowManyWords.py
@@ -0,0 +1,16 @@
+def HowManyWords(s):
+    answer = 0
+    string = 'word'
+    target = 0
+    for i in xrange(0, len(s)):
+        if s[i].lower() == string[target]:
+            target += 1
+            if target == 4:
+                answer += 1
+                target = 0
+
+    return answer
+
+
+
+print HowManyWords("awosdrd")
